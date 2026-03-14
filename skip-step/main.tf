7b58872b84f7377f136b0d466e4d922cdf61f683
Ahmedsayed86 <65818138+Ahmedsayed86@users.noreply.github.com>
2026-03-14 15:21:27 +0300

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -1,15 +1,24 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.0"
+    }
+  }
+
   backend "azurerm" {
+    resource_group_name  = "tfstate-rg"       # اسم الـ RG اللي هتحط فيها الـ storage account
+    storage_account_name = "tfstatestorage"   # اسم الـ Storage Account اللي هيحفظ فيها الـ TF state
+    container_name       = "tfstate"          # اسم الـ container داخل الـ storage account
+    key                  = "terraform.tfstate" # اسم ملف الـ state
   }
 }
 
 provider "azurerm" {
-  version = ">=2.0"
-  # The "feature" block is required for AzureRM provider 2.x.
   features {}
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "resourcegroup-test-tbd-3"
-  location = "westeurope"
+  location = "CanadaCentral"
 }
